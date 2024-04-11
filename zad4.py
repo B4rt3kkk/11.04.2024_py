5fe121c38cf5f580c48dfa8f30dac056714e7d2c
2CT <maksszkorupa@Interia.pl>
2024-04-11 11:30:15 +0200

    lala

---
+++ zad4.py
@@ -0,0 +1,10 @@
+sum = 0
+count = 0
+
+while sum <= 123:
+    a = int(input("Wprowadź liczby 1 lub 2 cyfrowa naturalna: "))
+    sum += a
+    count += 1
+
+print(" Liczba składników :", count)
+print("suma:", sum)
